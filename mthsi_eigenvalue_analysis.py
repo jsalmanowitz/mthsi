bug fixes for metric calculation and automation improvements for eigenvalue analysis

mthsi_eigenvalue_analysis.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# custom code imports
+import utils.read as read
+import utils.eigencomp as eigencomp
 
 #%%
 def sort_mthsi_df(df):
@@ -15,71 +18,53 @@ def sort_mthsi_df(df):
 
 
 #%% load in data
-veg_isomap_eigenvalues = pd.read_csv('city_isomap_eigenvalues.csv')
-veg_laplacian_eigenvalues = pd.read_csv('city_laplacian_eigenvalues.csv')
-veg_lle_eigenvalues = pd.read_csv('city_lle_eigenvalues.csv')
-veg_pca_eigenvalues = pd.read_csv('city_pca_eigenvalues.csv')
+veg_isomap_eigenvalues = pd.read_csv('veg_isomap_eigenvalues.csv')
+veg_laplacian_eigenvalues = pd.read_csv('veg_laplacian_eigenvalues.csv')
+veg_lle_eigenvalues = pd.read_csv('veg_lle_eigenvalues.csv')
+veg_pca_eigenvalues = pd.read_csv('veg_pca_eigenvalues.csv')
 # %% sort data
 veg_isomap_dict = sort_mthsi_df(veg_isomap_eigenvalues)
 veg_laplacian_dict = sort_mthsi_df(veg_laplacian_eigenvalues)
 veg_lle_dict = sort_mthsi_df(veg_lle_eigenvalues)
 veg_pca_dict = sort_mthsi_df(veg_pca_eigenvalues)
 
-#%% functions to move later
-
-def get_metrics_from_list(eigen_list,metric):
-
-    def get_cos_sim(eigen_list_left,eigen_list_right):
-        cos_vals = []
-        for index in range(len(eigen_list_left)):
-            vec1 = eigen_list_left[index]
-            vec2 = eigen_list_right[index]
-            cos_sim = np.dot(vec1,vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
-            cos_vals += [cos_sim]
-        return np.array(cos_vals[1:-1])
-    
-    def get_sam(eigen_list_left,eigen_list_right):
-        cos_sim = get_cos_sim(eigen_list_left,eigen_list_right)
-        return np.arccos(cos_sim)*180/np.pi
-    
-    def get_euclidean(eigen_list_left,eigen_list_right):
-        diff = eigen_list_left-eigen_list_right
-        return np.linalg.norm(diff,axis=1)[1:-1]
-    
-    def get_manhattan(eigen_list_left,eigen_list_right):
-        diff = eigen_list_left-eigen_list_right
-        return np.sum(abs(diff),axis=1)[1:-1]
-
-    nan_array = np.full(len(eigen_list[0]), np.nan)
-    nan_array = nan_array[np.newaxis,:]
-    eigen_list_left = np.concatenate([nan_array,eigen_list])
-    eigen_list_right = np.concatenate([eigen_list,nan_array])
-
-    if metric == 'cosine':
-        return get_cos_sim(eigen_list_left,eigen_list_right)
-    
-    elif metric == 'sam':
-        return get_sam(eigen_list_left,eigen_list_right)
-    
-    elif metric == 'euclidean':
-        return get_euclidean(eigen_list_left,eigen_list_right)
-    
-    elif metric == 'manhattan':
-        return get_manhattan(eigen_list_left,eigen_list_right)
-
-    
 # %%
-veg_isomap_eigens = veg_isomap_dict['Eigenvalues'][1::]
-veg_laplacian_eigens = veg_laplacian_dict['Eigenvalues'][1::]
-veg_lle_eigens = veg_lle_dict['Eigenvalues'][1::]
-veg_pca_eigens = veg_pca_dict['Eigenvalues'][1::]
+# for veg, use all data; for city, [1::]
+veg_isomap_eigens = veg_isomap_dict['Eigenvalues']
+veg_laplacian_eigens = veg_laplacian_dict['Eigenvalues']
+veg_lle_eigens = veg_lle_dict['Eigenvalues']
+veg_pca_eigens = veg_pca_dict['Eigenvalues']
+
+#%%
+mode = 'start'
+
+# for veg scenes
+match mode:
+    case 'diff':
+        time = [2,4,6,8]
+    case 'start':
+        time = [0,2,4,6,8]
+    case 'end':
+        time = [0,2,4,6,8]
+
+# for city scenes
+"""
+match mode:
+    case 'diff':
+        time = [2,4,6,8]
+    case 'start':
+        time = [0,2,4,6,8]
+    case 'end':
+        time = [0,2,4,6,8]
+"""
+
+print(time)
 
 for metric in ['sam','euclidean','manhattan']:
-    iso_metric = get_metrics_from_list(veg_isomap_eigens,metric=metric)
-    lap_metric = get_metrics_from_list(veg_laplacian_eigens,metric=metric)
-    lle_metric = get_metrics_from_list(veg_lle_eigens,metric=metric)
-    pca_metric = get_metrics_from_list(veg_pca_eigens,metric=metric)
-    time = [0,2,4,6]
+    iso_metric = eigencomp.get_metrics_from_list(veg_isomap_eigens,metric=metric,mode=mode)
+    lap_metric = eigencomp.get_metrics_from_list(veg_laplacian_eigens,metric=metric,mode=mode)
+    lle_metric = eigencomp.get_metrics_from_list(veg_lle_eigens,metric=metric,mode=mode)
+    pca_metric = eigencomp.get_metrics_from_list(veg_pca_eigens,metric=metric,mode=mode)
 
     plt.figure()
     plt.plot(time,iso_metric,label='ISOMAP',marker='o')
@@ -87,16 +72,8 @@ for metric in ['sam','euclidean','manhattan']:
     plt.plot(time,lle_metric,label='LLE',marker='o')
     plt.plot(time,pca_metric,label='PCA',marker='o')
     plt.xlabel('Time [weeks]')
-    plt.ylabel(f'Change in {metric} distance')
+    plt.ylabel(f'Change from {mode} of {metric} distance')
     plt.legend()
 
-    plt.figure()
-    plt.plot(time,iso_metric/iso_metric[0],label='ISOMAP',marker='o')
-    plt.plot(time,lap_metric/lap_metric[0],label='Laplacian Eigenmaps',marker='o')
-    plt.plot(time,lle_metric/lle_metric[0],label='LLE',marker='o')
-    plt.plot(time,pca_metric/pca_metric[0],label='PCA',marker='o')
-    plt.xlabel('Time [weeks]')
-    plt.ylabel(f'Normalized Change in {metric} distance')
-    plt.legend()
 
 # %%

utils/eigencomp.py
@@ -36,7 +36,7 @@ def get_metrics_from_list(eigen_list,metric,mode='diff'):
             vec2 = eigen_list_right[index]
             cos_sim = np.dot(vec1,vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
             cos_vals += [cos_sim]
-        return np.array(cos_vals[1:-1])
+        return np.array(cos_vals)
     
     def get_sam(eigen_list_left,eigen_list_right):
         cos_sim = get_cos_sim(eigen_list_left,eigen_list_right)
@@ -44,32 +44,34 @@ def get_metrics_from_list(eigen_list,metric,mode='diff'):
     
     def get_euclidean(eigen_list_left,eigen_list_right):
         diff = eigen_list_left-eigen_list_right
-        return np.linalg.norm(diff,axis=1)[1:-1]
+        return np.linalg.norm(diff,axis=1)
     
     def get_manhattan(eigen_list_left,eigen_list_right):
         diff = eigen_list_left-eigen_list_right
-        return np.sum(abs(diff),axis=1)[1:-1]
+        return np.sum(abs(diff),axis=1)
 
     if mode == 'diff':
         nan_array = np.full(len(eigen_list[0]), np.nan)
         nan_array = nan_array[np.newaxis,:]
-        eigen_list_left = np.concatenate([nan_array,eigen_list])
-        eigen_list_right = np.concatenate([eigen_list,nan_array])
+        eigen_list_left = np.concatenate([nan_array,eigen_list])[1:-1]
+        eigen_list_right = np.concatenate([eigen_list,nan_array])[1:-1]
     elif mode == 'start':
-        start_array = np.full(len(eigen_list[0]), eigen_list[0])
+        start_array = eigen_list[0]
         start_array = start_array[np.newaxis,:]
-        eigen_list_left = np.concatenate([start_array,eigen_list])
+        eigen_list_left = eigen_list
         eigen_list_right = start_array
-        for i in range(len(eigen_list)):
+        for i in range(len(eigen_list)-1):
             eigen_list_right = np.concatenate([eigen_list_right,start_array])
-
     elif mode == 'end':
-        end_array = np.full(len(eigen_list[0]), eigen_list[0])
-        end_array = start_array[np.newaxis,:]
-        eigen_list_left = np.concatenate([end_array,eigen_list])
+        end_array = eigen_list[-1]
+        end_array = end_array[np.newaxis,:]
+        eigen_list_left = eigen_list
         eigen_list_right = end_array
-        for i in range(len(eigen_list[1])):
+        for i in range(len(eigen_list)-1):
             eigen_list_right = np.concatenate([eigen_list_right,end_array])
+    else:
+        print('Mode does not exist.')
+        return None
 
     if metric == 'cosine':
         return get_cos_sim(eigen_list_left,eigen_list_right)