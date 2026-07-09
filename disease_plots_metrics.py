36f65ff3be940b42ba76b91ebd386003229f1239
jsalmanowtiz <js9002@g.rit.edu>
2026-07-09 17:53:44 -0400

    workflow for stats on healthy veg

--- disease_plots_metrics.py
+++ disease_plots_metrics.py
@@ -25,16 +25,52 @@ plt.title('Plot 005 Manhattan Difference Over Time')
 # %% get saved disease data
 import glob
 import numpy as np
-file_path = "/home/js9002/mthsi/*disease*.npy"
+from utils import eigencomp
+import matplotlib.pyplot as plt
+
+file_path = "/home/js9002/mthsi/*disease_plot_isos*.npy"
 disease_files = sorted(glob.glob(file_path))
+file_path_sev = "/home/js9002/mthsi/*disease_severity*.npy"
+disease_severity_files = sorted(glob.glob(file_path_sev))
+file_path_healthy = "/home/js9002/mthsi/*healthy_plot_isos*.npy"
+healthy_files = sorted(glob.glob(file_path_healthy))
+file_path_healthy_diff  = "/home/js9002/mthsi/*healthy_plot_diff*.npy"
+healthy_diff_files = sorted(glob.glob(file_path_healthy_diff))
+#%% prepare data
+d5_baseline = np.load(healthy_files[0])
+d5_baseline = d5_baseline/np.linalg.norm(d5_baseline)
+
+d5_disease_severity = []
+for plot in disease_severity_files:
+    sev = np.load(plot)
+    sev_d5 = sev[4]
+    d5_disease_severity += [sev_d5]
 
 d5_eigens = []
 for plot in disease_files:
     eigens = np.load(plot)
-    eigens_D5 = eigens[4]
+    eigens_D5 = eigens[4]/np.linalg.norm(eigens[4])
     d5_eigens += [eigens_D5]
 
+eigen_diff = eigencomp.get_metrics_from_list(d5_eigens,'euclidean',mode='compare',compare_list=d5_baseline)
+
+#%% generate stats
+
+diffs = np.load(healthy_diff_files[0])
+mean_diff = np.mean(diffs)
+std_diff = np.std(diffs)
+
+plt.figure()
+plt.hist(diffs, bins=30)
 
 
+#%%
+plt.figure()
+plt.scatter(d5_disease_severity,eigen_diff)
+plt.axhline(mean_diff,label='healthy mean',color='red')
+plt.axhline(mean_diff+2*std_diff,label='healthy 95% CI',color='blue')
+plt.xlabel('Disease Severity %')
+plt.ylabel('Euclidean Distance')
+plt.legend()
 
 # %%
